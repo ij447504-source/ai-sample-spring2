c8fc16088cf2a98d32ae980c08205d6ff5707cae
metacoding <getinthere@naver.com>
2026-03-19 09:37:50 +0900

    h2 console 설정완료

--- src/main/java/com/example/demo/_core/config/AppConfig.java
+++ src/main/java/com/example/demo/_core/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo._core.config;
 
+import org.h2.server.web.JakartaWebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,5 +14,13 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ServletRegistrationBean<JakartaWebServlet> h2ConsoleServlet() {
+        ServletRegistrationBean<JakartaWebServlet> bean = new ServletRegistrationBean<>(new JakartaWebServlet(),
+                "/h2-console/*");
+        bean.setLoadOnStartup(1);
+        return bean;
     }
 }
